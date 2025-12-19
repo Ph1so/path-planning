85619a49de118c6091b0e9f1b63f13edac08f555
Phi Nguyen <phiwe3296@gmail.com>
2025-12-19 17:02:57 -0500

    Boston example

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import copy
 import math
+import heapq
 from dataclasses import dataclass
 from typing import TypeAlias
 
@@ -14,19 +15,20 @@ from IPython.display import HTML
 ox.settings.use_cache = True
 ox.settings.cache_folder = './cache' 
 
-address = "Cary Memorial Library, 1874 Massachusetts Ave, Lexington, MA 02420"
+address = "Institute of Contemporary Art, 25 Harbor Shore Dr, Boston, MA 02210"
+place = "Boston, MA"
 
 try:
-    G = ox.graph_from_address(address, dist=800, network_type="drive")
+    G = ox.graph_from_address(place, dist = 2000, network_type="drive")
     start_node = ox.distance.nearest_nodes(G, X=-71.2299, Y=42.4479)
     print(start_node)
-    end_node = ox.distance.nearest_nodes(G, X=-71.2328, Y=42.4429)
+    end_node = ox.distance.nearest_nodes(G, X=-71.00328, Y=42.343929)
     print(end_node)
     node_colors = ['red' if node == start_node else 'green' if node == end_node else 'white' for node in G.nodes()]
     node_sizes = [100 if node == start_node or node == end_node  else 15 for node in G.nodes()]
 
     adj_dict = {
-        int(u): {str(v): round(float(d[0]['length']), 2) for v, d in nbrs.items()}
+        int(u): {int(v): round(float(d[0]['length']), 2) for v, d in nbrs.items()}
         for u, nbrs in G.adj.items()
     }
 
